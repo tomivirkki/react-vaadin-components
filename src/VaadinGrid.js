7e91953eeb8066ea851657f2102497a808a2c93f
Tomi Virkki <virkki@vaadin.com>
2018-07-21 16:14:10 +0300

    Add grid tree toggle

--- src/VaadinGrid.js
+++ src/VaadinGrid.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import '@vaadin/vaadin-grid';
 import '@vaadin/vaadin-grid/vaadin-grid-column';
+import '@vaadin/vaadin-grid/vaadin-grid-tree-toggle';
 
 const e = React.createElement;
 
@@ -9,20 +10,32 @@ export class VaadinGrid extends Component {
 
   render() {
     return e('vaadin-grid', {
+      theme: this.props.theme,
+      class: this.props.className,
       ref: g => {
+        if (!g) {
+          return;
+        }
+        g.heightByRows = this.props.heightByRows;
         g.items = this.props.items;
+        g.size = this.props.size;
+        g.dataProvider = this.props.dataProvider;
+        g.expandedItems = this.props.expandedItems;
 
         // TODO: Switch to using a renderers once they're available.
-        const ui = g._updateItem;
-        g._updateItem = (row, item) => {
-          ui.call(g, row, item);
-          Array.from(row.children).forEach((cell, index) => {
-            ReactDOM.render(cell._column.renderer(item), cell._content);
-            if (cell._column._headerCell && cell._column.header) {
-              // FIXME: Not the best place to do this as it's run quite often.
-              ReactDOM.render(cell._column.header, cell._column._headerCell._content);
-            }
-          });
+        if (!g._monkeyPatched) {
+          const ui = g._updateItem;
+          g._updateItem = (row, item) => {
+            ui.call(g, row, item);
+            Array.from(row.children).forEach((cell, index) => {
+              ReactDOM.render(cell._column.renderer(item), cell._content);
+              if (cell._column._headerCell && cell._column.header) {
+                // FIXME: Not the best place to do this as it's run quite often.
+                ReactDOM.render(cell._column.header, cell._column._headerCell._content);
+              }
+            });
+          }
+          g._monkeyPatched = true;
         }
       }
     }, this.props.children);
@@ -35,7 +48,28 @@ export class VaadinGridColumn extends Component {
     const headerTemplate =  e('template', {className: 'header'});
 
     return e('vaadin-grid-column', {
-      ref: c => Object.assign(c, this.props)
+      ref: c => c && Object.assign(c, this.props)
     }, this.props.header ? headerTemplate : '');
+  }
+}
+
+export class VaadinGridTreeToggle extends Component {
+  render() {
+    return e('vaadin-grid-tree-toggle', {
+      ref: t => {
+        if (!t) {
+          return
+        }
+        t.leaf = this.props.leaf;
+        t.expanded = this.props.expanded;
+
+        if (!t._hasExpandedListener) {
+          t.addEventListener('expanded-changed', e => {
+            this.props.expandedChanged(e.detail.value);
+          });
+          t._hasExpandedListener = true;
+        }
+      }
+    }, this.props.children);
   }
 }
