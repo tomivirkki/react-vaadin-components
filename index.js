315f6a7cb39870eee52efeb504bab1ba0ac2ff22
Tomi Virkki <virkki@vaadin.com>
2018-08-19 13:30:44 +0300

    Add vaadin-button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import '@webcomponents/webcomponentsjs';
 import './custom-elements-es5-adapter';
 export * from './src/VaadinGrid';
+export * from './src/VaadinButton';
---
+++ src/VaadinButton.js
@@ -0,0 +1,10 @@
+import VaadinComponent from './VaadinComponent';
+import '@vaadin/vaadin-button';
+
+export class VaadinButton extends VaadinComponent {
+
+  constructor() {
+    super('vaadin-button');
+  }
+
+}
