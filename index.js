6ac6d8a29d1a8b311118ef268ff4211477818779
Tomi Virkki <virkki@vaadin.com>
2018-08-19 20:51:37 +0300

    Add text field

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import '@webcomponents/webcomponentsjs';
 import './custom-elements-es5-adapter';
 export * from './src/VaadinGrid';
 export * from './src/VaadinButton';
+export * from './src/VaadinTextField';
---
+++ src/VaadinTextField.js
@@ -0,0 +1,10 @@
+import VaadinComponent from './VaadinComponent';
+import '@vaadin/vaadin-text-field';
+
+export class VaadinTextField extends VaadinComponent {
+
+  constructor() {
+    super('vaadin-text-field');
+  }
+
+}
