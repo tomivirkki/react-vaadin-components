e19ce45a5e721e0191ca34c61ebc24f79fc5ac90
Tomi Virkki <virkki@vaadin.com>
2018-08-27 19:52:14 +0300

    Export components separately

---
+++ components.js
@@ -0,0 +1,5 @@
+export * from './src/VaadinGrid';
+export * from './src/VaadinButton';
+export * from './src/VaadinTextField';
+export * from './src/VaadinCheckbox';
+export * from './src/VaadinRadioButton';
--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 import '@webcomponents/webcomponentsjs';
 import './custom-elements-es5-adapter';
-export * from './src/VaadinGrid';
-export * from './src/VaadinButton';
-export * from './src/VaadinTextField';
-export * from './src/VaadinCheckbox';
-export * from './src/VaadinRadioButton';
+export * from './components';
