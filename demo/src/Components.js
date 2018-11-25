843a225427c8de5ccd781b8e6debe058c8c4ca58
Tomi Virkki <virkki@vaadin.com>
2018-11-25 18:09:34 +0200

    Reuse vaadin demos

--- demo/src/Components.js
+++ demo/src/Components.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Components.css';
-import demos from './demos';
+import demos, { loadCode } from './demos.js';
 import Playground from 'component-playground';
 import { Tree } from './Tree';
 import { Redirect } from 'react-router-dom';
@@ -15,24 +15,35 @@ export class Components extends Component {
 
   itemSelected = item => {
     if (!item.parent) {
-      debugger;
+      return;
     }
 
     this.setState({
-    redirectTo: `${this.props.match.path.split('/:')[0]}/${item.parent.id}/${item.id}`,
-    menuOpen: false
-  })
-}
+      redirectTo: `${this.props.match.path.split('/:')[0]}/${item.parent.id}/${item.id}`,
+      menuOpen: false
+    })
+  }
 
-  render() {
-    const params = this.props.match.params;
+  componentWillReceiveProps = props => {
+    const params = props.match.params;
     const component = demos.filter(component => component.id === params.component)[0] || demos[0];
     const demo = component.children.filter(demo => demo.id === params.demo)[0] || component.children[0];
 
-    return <VaadinHorizontalLayout style={{height: '100%', position: 'relative'}}>
+    if (demo.code) {
+      this.setState({demo});
+    } else {
+      loadCode(demo).then(() => this.setState({demo}));
+    }
+  }
+
+
+  render() {
+    const demo = this.state.demo;
+
+    return demo ? <VaadinHorizontalLayout style={{height: '100%', position: 'relative'}}>
       <VaadinVerticalLayout style={{flex: 1, overflow: 'auto'}} theme="padding">
         <h2>{demo.parent.title + ' – ' + demo.title}</h2>
-        <Playground noRender={!demo.render} codeText={demo.code || ''} scope={demo.scope || {}}/>
+        <Playground noRender={!demo.render} codeText={demo.code || ''} scope={demo.scope}/>
       </VaadinVerticalLayout>
 
       <VaadinVerticalLayout className={`Menubar${this.state.menuOpen ? ' open' : ''}`} theme="padding">
@@ -41,7 +52,7 @@ export class Components extends Component {
 
       <div className="Menutoggle" onClick={() => this.setState({menuOpen: !this.state.menuOpen})}></div>
       {this.state.redirectTo && <Redirect to={this.state.redirectTo} push />}
-    </VaadinHorizontalLayout>;
+    </VaadinHorizontalLayout> : null;
   }
 
 }
--- demo/src/demos.js
+++ demo/src/demos.js
@@ -2,230 +2,65 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import * as components from 'react-vaadin-components';
 import users from './users.js';
+import demos from './demos.json';
+
 const scope = {React, ReactDOM, Component, users, ...components};
-
-// Demo build helpers
-
-/*
-Grid:
-
-Other convenience columns
-Renderers
-Theme variants
-footer
-reordering and resizing
-sorting and filtering
-
-row details
-Tree grid
-Active item
-*/
-
-const getIndent = spaces =>  Array(spaces).join(' ');
-
-const defaultIndent = 6;
-
-const buildColumn = ({path, header, textAlign, attributes, spaces} = {}) => {
-  attributes = attributes || [];
-  textAlign && attributes.push(`textAlign="${textAlign}"`)
-  spaces = spaces || defaultIndent + 2;
-  const isIndexColumn = header === '#';
-
-  if (isIndexColumn) {
-    attributes.push(`header="#"`);
-    attributes.push(`renderer={({index}) => <div>{index}</div>}`);
-  } else {
-    if (header) {
-      attributes.push(`header="${header}"`);
-    }
-    if (path) {
-      attributes.push(`path="${path}"`);
-    }
-  }
-
-  if (attributes.filter(a => a.indexOf('renderer') > -1)[0]) {
-    return `${getIndent(spaces)}<VaadinGridColumn ${attributes ? ('\n'+ getIndent(spaces + 2) + attributes.join('\n'+ getIndent(spaces + 2))) : ''}>${'\n' + getIndent(spaces)}</VaadinGridColumn>`;
-  } else {
-    return `${getIndent(spaces)}<VaadinGridColumn ${attributes ? (attributes.join(' ')) : ''}></VaadinGridColumn>`;
-  }
-
-};
-
-const buildSelectionColumn = ({spaces, attributes} = {}) => {
-  attributes = attributes || [];
-  attributes.push('autoSelect');
-  spaces = spaces || defaultIndent + 2;
-  return getIndent(spaces) + `<VaadinGridSelectionColumn ${attributes.join(' ')}></VaadinGridSelectionColumn>`;
-}
-
-const buildSortColumn = ({spaces, attributes} = {}) => {
-  attributes = attributes || [];
-  spaces = spaces || defaultIndent + 2;
-  return getIndent(spaces) + `<VaadinGridSortColumn ${attributes.join(' ')}></VaadinGridSortColumn>`;
-}
-
-const buildGrid = (attributes, content, spaces = defaultIndent) => {
-  return `${getIndent(spaces)}<VaadinGrid ${attributes}>${'\n' + content.join('\n') + '\n'}${getIndent(spaces)}</VaadinGrid>`;
-}
-
-const buildColumnGroup = (attributes, content, spaces = defaultIndent + 2) => {
-  return `\n${getIndent(spaces)}<VaadinGridColumnGroup ${attributes}>${'\n' + content.join('\n') + '\n'}${getIndent(spaces)}</VaadinGridColumnGroup>`;
-}
-
-const buildGroupedGridDemo = ({resizable, reorderable, frozenColumns} = {}) => {
-  return buildGrid('items={users}'.concat(reorderable ? ' columnReorderingAllowed' : ''), [
-    buildSelectionColumn({attributes: frozenColumns ? ['frozen'] : []}),
-    buildColumn({path: 'username', attributes: ['width="170px"', 'flexGrow="0"'].concat(frozenColumns ? ['frozen'] : [])}),
-    buildColumn({path: 'email', attributes: ['width="calc(210px + 10%)"', 'flexGrow="0"'].concat(resizable ? ['resizable'] : [])}),
-    buildColumnGroup('header="Name"'.concat(resizable ? ' resizable' : ''), [
-      buildColumn({path: 'name.first', attributes: ['width="130px"', 'flexGrow="0"'], spaces: defaultIndent + 4}),
-      buildColumn({path: 'name.last', attributes: ['width="180px"', 'flexGrow="0"'], spaces: defaultIndent + 4}),
-    ]),
-    buildColumnGroup('header="Address"'.concat(resizable ? ' resizable' : ''), [
-      buildColumn({path: 'location.street', attributes: ['width="25%"', 'flexGrow="0"'], spaces: defaultIndent + 4}),
-      buildColumn({path: 'location.city', attributes: ['width="15%"', 'flexGrow="0"'], spaces: defaultIndent + 4}),
-      buildColumn({path: 'location.state', spaces: defaultIndent + 4})
-    ]),
-    ])
-}
-
-const demos = [
-  {title: 'Grid', scope, children: [
-    {title: 'Items and Columns',
-    code: buildGrid('items={users}', [
-      buildColumn({path: 'name.first', header: 'First Name'}),
-      buildColumn({path: 'name.last', header: 'Last Name'}),
-      buildColumn({path: 'location.city'}),
-      buildColumn({path: 'visitCount', textAlign: 'end', width: '100px', flexGrow: '0'})
-      ])
-    },
-    {title: 'Lazy Loading Data', render: true, code: `
-      class ComponentExample extends Component {
-        render() {
-          return (${'\n' + buildGrid('dataProvider={this.dataProvider}', [
-            buildColumn({path: 'firstName', spaces: defaultIndent + 8}),
-            buildColumn({path: 'lastName', spaces: defaultIndent + 8}),
-            buildColumn({path: 'email', spaces: defaultIndent + 8}),
-            ], defaultIndent + 6)}
-          );
-        }
-
-        dataProvider = (params, callback) => {
-          const KNOWN_DATA_SIZE = 200;
-          const index = params.page * params.pageSize;
-
-          fetch('https://demo.vaadin.com/demo-data/1.0/people?index=' + index + '&count=' + params.pageSize)
-            .then(res => res.json())
-            .then(json => callback(json.result, KNOWN_DATA_SIZE));
-        }
-      }
-
-      ReactDOM.render(<ComponentExample/>, mountNode);
-    `},
-    {title: 'Selecting', code: buildGrid('items={users}', [
-      buildSelectionColumn(),
-      buildColumn({path: 'name.first', header: 'First Name'}),
-      buildColumn({path: 'name.last', header: 'Last Name'}),
-      buildColumn({path: 'email'})
-      ])
-    },
-    {title: 'Sorting', code: buildGrid('items={users}', [
-      buildSortColumn({attributes: ['path="name.first"', 'header="First Name"']}),
-      buildSortColumn({attributes: ['path="name.last"', 'header="Last Name"']}),
-      buildSortColumn({attributes: ['path="location.city"']}),
-      buildColumn({path: 'email'})
-      ])
-    },
-    {title: 'Column Groups',
-      code: buildGroupedGridDemo()
-    },
-    {title: 'Frozen Columns',
-      code: buildGroupedGridDemo({frozenColumns: true})
-    },
-    {title: 'Reordering Columns',
-      code: buildGroupedGridDemo({reorderable: true, frozenColumns: true})
-    },
-    {title: 'Resizing Columns',
-      code: buildGroupedGridDemo({resizable: true, frozenColumns: true})
-    },
-  ]},
-  {title: 'Button', scope, children: [
-    {title: 'Basic Button', code: `
-    <VaadinButton onClick={console.log}>Click Me</VaadinButton>
-    `},
-    {title: 'Theme Variants', code: `
-    <div>
-      <VaadinButton onClick={console.log}>Click Me</VaadinButton>
-
-      <VaadinButton onClick={console.log} theme="primary">Click Me</VaadinButton>
-
-      <VaadinButton onClick={console.log} theme="error">Click Me</VaadinButton>
-
-      <VaadinButton onClick={console.log} theme="success">Click Me</VaadinButton>
-
-      <VaadinButton onClick={console.log} theme="small">Click Me</VaadinButton>
-    </div>
-    `}
-  ]},
-  {title: 'Text Field', scope, children: [
-    {title: 'Text Field', code: `
-    <VaadinTextField label='First Name'></VaadinTextField>
-    `},
-    {title: 'Text Area', code: `
-    <VaadinTextArea label="Description"></VaadinTextArea>
-    `},
-    {title: 'Password Field', code: `
-    <VaadinPasswordField label="Password"></VaadinPasswordField>
-    `},
-  ]},
-  {title: 'Checkbox', scope, children: [
-    {title: 'Checkbox', code: `
-    <VaadinCheckbox>Subscribe</VaadinCheckbox>
-    `}
-  ]},
-  {title: 'Radio Button', scope, children: [
-    {title: 'Radio Button Group', code: `
-    <VaadinRadioGroup>
-      <VaadinRadioButton>One</VaadinRadioButton>
-      <VaadinRadioButton>Two</VaadinRadioButton>
-      <VaadinRadioButton>Three</VaadinRadioButton>
-    </VaadinRadioGroup>
-    `}
-  ]},
-  {title: 'Date Picker', scope, children: [
-    {title: 'Date Picker', code: `
-    <VaadinDatePicker></VaadinDatePicker>
-    `}
-  ]},
-  {title: 'Combo Box', scope, children: [
-    {title: 'Combo Box', code: `
-    <VaadinComboBox
-      items={users}
-      itemLabelPath="username"
-      itemValuePath="username">
-    </VaadinComboBox>
-    `}
-  ]}
-]
 
 function getId(title) {
   return title.toLowerCase().replace(/ /g, '-');
 }
 
-function addDemoIds(demos, parent) {
+function prepareDemos(demos, parent) {
   demos.forEach(demo => {
+    demo.scope = scope;
     demo.id = getId(demo.title);
-    demo.children && addDemoIds(demo.children, demo);
+    demo.children && prepareDemos(demo.children, demo);
     demo.parent = parent;
   });
 }
 
-addDemoIds(demos);
-
-demos.forEach(component => {
-  component.children.forEach(page => {
-    page.scope = component.scope;
-  })
-});
+prepareDemos(demos);
 
 export default demos;
+
+export const loadCode = async demo => {
+  return new Promise((resolve, reject) => {
+    import('./demos/' + demo.codeSource)
+      .then(res => res.default)
+      .then(fetch)
+      .then(res => res.text())
+      .then(html => {
+        const dom = document.createElement('div');
+        dom.innerHTML = html;
+        const mainTemplate = dom.querySelector('template');
+        const title = mainTemplate.content.querySelector('h3');
+
+        demo.description = [];
+        let demoSnippet = title.nextSibling;
+        while (demoSnippet.localName !== 'vaadin-demo-snippet') {
+          if (demoSnippet.localName === 'p') {
+            demo.description.push(<p key={demoSnippet.textContent}>{demoSnippet.textContent}</p>);
+          }
+          demoSnippet = demoSnippet.nextSibling;
+        }
+
+        const demoContent = demoSnippet.querySelector('template').content;
+        const wrapper = document.createElement('div');
+        wrapper.appendChild(demoContent);
+        const demoScript = wrapper.querySelector('script');
+        demoScript && wrapper.removeChild(demoScript);
+
+        demo.code = wrapper.children.length === 1 ? wrapper.innerHTML : `<div>${wrapper.innerHTML}</div>`;
+
+        demo.code = demo.code
+          .replace(/vaadin/g, 'Vaadin')
+          .replace(/-[a-z]/g, match => match.toUpperCase())
+          .replace(/-/g, '');
+
+        demo.replacements && demo.replacements.forEach(r => {
+          demo.code = demo.code.replace(new RegExp(r.from, 'g'), r.to);
+        });
+        resolve(demo);
+      });
+    });
+}
